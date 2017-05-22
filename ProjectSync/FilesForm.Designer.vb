67c385f2081661e38a35946b3a1c08d39c0a2c81
strider757 <dmitry.usov.ne@gmail.com>
2017-05-22 16:01:26 +0300

    v.001

--- ProjectSync/FilesForm.Designer.vb
+++ ProjectSync/FilesForm.Designer.vb
@@ -22,58 +22,109 @@ Partial Class FilesForm
     'Не изменяйте ее в редакторе исходного кода.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.But_add = New System.Windows.Forms.Button()
+        Me.But_save = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.name = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.location = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.but_addFilter = New System.Windows.Forms.Button()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'But_add
+        '
+        Me.But_add.Location = New System.Drawing.Point(12, 323)
+        Me.But_add.Name = "But_add"
+        Me.But_add.Size = New System.Drawing.Size(111, 28)
+        Me.But_add.TabIndex = 2
+        Me.But_add.Text = "Добавить"
+        Me.But_add.UseVisualStyleBackColor = True
+        '
+        'But_save
+        '
+        Me.But_save.Location = New System.Drawing.Point(129, 323)
+        Me.But_save.Name = "But_save"
+        Me.But_save.Size = New System.Drawing.Size(111, 28)
+        Me.But_save.TabIndex = 3
+        Me.But_save.Text = "Сохранить"
+        Me.But_save.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(6, 19)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(118, 21)
+        Me.ComboBox1.TabIndex = 5
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(578, 323)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(111, 28)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Очистить таблицу"
+        Me.Button2.UseVisualStyleBackColor = True
         '
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.name, Me.location})
         Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders
-        Me.DataGridView1.Size = New System.Drawing.Size(677, 301)
-        Me.DataGridView1.TabIndex = 0
+        Me.DataGridView1.Size = New System.Drawing.Size(677, 291)
+        Me.DataGridView1.TabIndex = 7
         '
-        'name
+        'GroupBox1
         '
-        Me.name.HeaderText = "name"
-        Me.name.Name = "name"
+        Me.GroupBox1.Controls.Add(Me.but_addFilter)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(246, 309)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(208, 49)
+        Me.GroupBox1.TabIndex = 8
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Фильтр"
         '
-        'location
+        'but_addFilter
         '
-        Me.location.HeaderText = "location"
-        Me.location.Name = "location"
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(460, 335)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(111, 28)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.but_addFilter.Location = New System.Drawing.Point(130, 17)
+        Me.but_addFilter.Name = "but_addFilter"
+        Me.but_addFilter.Size = New System.Drawing.Size(68, 23)
+        Me.but_addFilter.TabIndex = 6
+        Me.but_addFilter.Text = "Ред. спис."
+        Me.but_addFilter.UseVisualStyleBackColor = True
         '
         'FilesForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(701, 370)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.DataGridView1)
-
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.But_save)
+        Me.Controls.Add(Me.But_add)
+        Me.Name = "FilesForm"
         Me.Text = "FilesForm"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents DataGridView1 As DataGridView
-    Friend WithEvents name As DataGridViewTextBoxColumn
-    Friend WithEvents location As DataGridViewTextBoxColumn
-    Friend WithEvents Button1 As Button
+    Friend WithEvents But_add As System.Windows.Forms.Button
+    Friend WithEvents But_save As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents but_addFilter As System.Windows.Forms.Button
 End Class
--- ProjectSync/FilesForm.vb
+++ ProjectSync/FilesForm.vb
@@ -1,12 +1,43 @@
 ﻿Public Class FilesForm
     Dim SyncFileName As String = "D:\Development\Work\ProjectSync\Config.xml"
+    Dim fileCollect As New ArrayList
     Dim xdoc As XDocument
 
     Private Sub FilesForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim str = "sfsf"
-        Dim var
 
+
+        With DataGridView1
+            .Columns.Add("name", "name")
+            .Columns.Add("location", "location")
+        End With
+
+        With OpenFileDialog1
+            .Multiselect = True
+            .Title = "Окно выбора файлов"
+            '.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"
+        End With
+
+        If Not xmlLoad() Then Me.Close()
     End Sub
+
+    '    Private Function xmlLoad() As Boolean 'загружаем хмл со списком файлов. При удачной загрузке возвращает true
+    '        On Error GoTo err1
+    '        Dim i As Integer = 0
+    '        xdoc = XDocument.Load(SyncFileName)
+    '        DataGridView1.Rows.Clear()
+    '        For Each xe As XElement In xdoc.Element("Root").Element("Files").Elements("File")
+    '            DataGridView1.Rows.Add()
+    '            DataGridView1.Rows.Item(i).Cells(0).Value = xe.Attribute("fname").Value
+    '            DataGridView1.Rows.Item(i).Cells(1).Value = xe.Value
+    '            i = i + 1
+    '        Next
+    '        xmlLoad = True
+    '        Exit Function
+    'err1:
+    '        MsgBox("Ошибка номер " & Err.Number & ". " & Err.Description, vbCritical, "Ошибка")
+    '        xmlLoad = False
+
+    '    End Function
 
     Private Function xmlLoad() As Boolean 'загружаем хмл со списком файлов. При удачной загрузке возвращает true
         On Error GoTo err1
@@ -14,9 +45,9 @@
         xdoc = XDocument.Load(SyncFileName)
         DataGridView1.Rows.Clear()
         For Each xe As XElement In xdoc.Element("Root").Element("Files").Elements("File")
-            DataGridView1.Rows.Add()
-            DataGridView1.Rows.Item(i).Cells(0).Value = xe.Attribute("fname").Value
-            DataGridView1.Rows.Item(i).Cells(1).Value = xe.Value
+            fileCollect.Add(New FileObj)
+            fileCollect.Item(i).Name = xe.Attribute("fname").Value
+            fileCollect.Item(i).Location = xe.Value
             i = i + 1
         Next
         xmlLoad = True
@@ -27,7 +58,31 @@ err1:
 
     End Function
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
         If Not xmlLoad() Then Me.Close()
+    End Sub
+
+    Private Sub But_add_Click(sender As Object, e As EventArgs) Handles But_add.Click
+        MsgBox(OpenFileDialog1.Filter)
+        OpenFileDialog1.ShowDialog()
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        DataGridView1.Rows.Clear()
+    End Sub
+
+    Private Sub ComboBox1_KeyDown(sender As Object, e As KeyEventArgs) Handles ComboBox1.KeyDown
+        If e.KeyCode = 13 Then
+            ComboBox1.Items.Add(ComboBox1.Text)
+        End If
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        OpenFileDialog1.Filter = "Filter| " & ComboBox1.Text
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
     End Sub
 End Class
