2dbe87dc317257b76f4a4590a688aea70d69dd60
strider757 <dmitry.usov.ne@gmail.com>
2017-05-23 07:53:59 +0300

    v.0001

---
+++ ProjectSync/FileObj.vb
@@ -0,0 +1,22 @@
+﻿Public Class FileObj
+    Private fName As String
+    Private fLocation As String
+
+    Public Property Name() As String
+        Get
+            Return fName
+        End Get
+        Set(value As String)
+            fName = value
+        End Set
+    End Property
+
+    Public Property Location() As String
+        Get
+            Return fLocation
+        End Get
+        Set(value As String)
+            fLocation = value
+        End Set
+    End Property
+End Class
