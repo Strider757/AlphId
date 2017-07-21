9afc75176db758195c7c365f93e9cfb57d17d2fc
strider757 <dmitry.usov.ne@gmail.com>
2017-07-21 15:58:05 +0300

    v0.5.5

--- ProjectSync/Form1.vb
+++ ProjectSync/Form1.vb
@@ -2,20 +2,26 @@
     Public xconfig As XDocument
     Dim parentNode As TreeNode
     Dim key As Integer = 1
+    Dim maxId As Integer
     Public confFullName As String = "C:\Users\user\Desktop\test38_id.xmlcfg"
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         xconfig = XDocument.Load(confFullName)
         addToTree(xconfig.Element("Configuration").Element("Signals"))
+        Label2.Text = maxId
     End Sub
+
     Sub addToTree(xe As XElement, Optional parent As TreeNode = Nothing)
 
         For Each x As XElement In xe.Element("Items").Elements("Item")
+
             If parent Is Nothing Then
-                parentNode = TreeView1.Nodes.Add(key, x.Attribute("Name"))
+                parentNode = TreeView1.Nodes.Add(CInt(x.Attribute("Id")), x.Attribute("Name"))
                 key = key + 1
+                If maxId < CInt(x.Attribute("Id")) Then maxId = CInt(x.Attribute("Id"))
             Else
-                parentNode = parent.Nodes.Add(key, x.Attribute("Name"))
+                parentNode = parent.Nodes.Add(CInt(x.Attribute("Id")), x.Attribute("Name"))
                 key = key + 1
+                If maxId < CInt(x.Attribute("Id")) Then maxId = CInt(x.Attribute("Id"))
             End If
 
             If x.Nodes.Any Then
@@ -23,15 +29,24 @@
             End If
 
         Next
+
     End Sub
 
 
 
     Private Sub TreeView1_NodeMouseClick(sender As Object, e As TreeNodeMouseClickEventArgs) Handles TreeView1.NodeMouseClick
-        Label1.Text = TreeView1.SelectedNode.FullPath
+        'Label1.Text = TreeView1.SelectedNode.FullPath
     End Sub
 
     Private Sub Form1_DragDrop(sender As Object, e As DragEventArgs) Handles Me.DragDrop
         MsgBox("драг")
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
     End Sub
 End Class
